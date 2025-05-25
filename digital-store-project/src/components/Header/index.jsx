59dbd24cba47f24d413b61b59fd8905f38e9e933
cauaosp <cauaosp20@gmail.com>
2025-05-24 23:24:24 -0300

    feat: cabeçalho para desktop

--- digital-store-project/src/components/Header/index.jsx
+++ digital-store-project/src/components/Header/index.jsx
@@ -1,24 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Logo from "../Logo";
 import { SidebarTrigger } from "../ui/sidebar";
+import SiteNavigation from "./SiteNavigation";
 
 const Header = () => {
   return (
-    <div className="bg-slate-300 py-8">
-      <div className="hidden md:flex items-center gap-2">
-        <SidebarTrigger />
-        <Logo />
-        <h1>Título</h1>
-        <h1>Menu de navegação</h1>
+    <>
+      <div className="hidden md:block items-center p-8 px-14">
+        <div className="flex justify-between items-center mb-4">
+          <Logo />
+          <div className="relative flex items-center">
+            <Input
+              className="md:w-60 lg:w-96 bg-stone-50"
+              placeholder="Pesquisar o produto..."
+            />
+            <img src="/Search.svg" alt="Lupa" className="absolute right-8" />
+          </div>
+          <div className="underline text-zinc-700">Cadastre-se</div>
+          <Button>Entrar</Button>
+          <img src="/Carbuy.svg" alt="Carrinho de compras" />
+        </div>
+        <SiteNavigation />
       </div>
-      <div className="flex justify-between md:hidden items-center gap-2 bg-white w-full px-5 ">
+      <div className="flex justify-between md:hidden items-center gap-2 bg-white w-full p-4 pr-2">
         <SidebarTrigger />
         <Logo />
-        <div className="flex gap-2">
-          <img src="/Search.svg" alt="Carrinho de compras" />
+        <div className="flex gap-4">
+          <img src="/Search.svg" alt="Lupa" />
           <img src="/Carbuy.svg" alt="Carrinho de compras" />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- digital-store-project/src/components/Logo/index.jsx
+++ digital-store-project/src/components/Logo/index.jsx
@@ -1,16 +1,16 @@
 const Logo = () => {
   return (
-    <div className="flex items-center gap-4">
+    <div>
       {/* Versão Desktop */}
       <div className="hidden md:flex items-center gap-2">
         <img src="./Logo.svg" alt="logo" className="h-8 w-8" />
-        <div className="text-2xl font-bold text-primary">Digital Store</div>
+        <div className="text-4xl font-bold text-primary">Digital Store</div>
       </div>
 
       {/* Versão Mobile */}
-      <div className="flex md:hidden items-center gap-2">
-        <img src="./Logo.svg" alt="logo" className="h-8 w-8" />
-        <div className="text-xl font-bold text-primary">Digital Store</div>
+      <div className="flex md:hidden items-center gap-3">
+        <img src="./Logo.svg" alt="logo" className="h-6 w-6" />
+        <div className="text-2xl font-bold text-primary">Digital Store</div>
       </div>
     </div>
   );
