97dbe568b170ebdeaf19533f7427605b619d7fae
cauaosp <cauaosp20@gmail.com>
2025-06-02 22:23:10 -0300

    feat: footer in dektop and mobile screens

--- digital-store-project/src/Layout.jsx
+++ digital-store-project/src/Layout.jsx
@@ -5,11 +5,11 @@ import Footer from "./components/Footer";
 
 const Layout = ({ children }) => {
   return (
-    <SidebarProvider className="flex-col container h-screen overflow-auto">
+    <SidebarProvider className="flex-col container h-screen">
       <AppSidebar />
       <Header />
       <main className="flex-1">{children}</main>
-      <Footer className="" />
+      <Footer />
     </SidebarProvider>
   );
 };
