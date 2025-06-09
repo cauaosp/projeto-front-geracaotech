89cb10811756dc4075b2cc693a374775ba9754f1
cauaosp <cauaosp20@gmail.com>
2025-06-09 11:33:04 -0300

    feat: add offer section

--- digital-store-project/src/pages/HomePage/index.jsx
+++ digital-store-project/src/pages/HomePage/index.jsx
@@ -1,15 +1,40 @@
+import Section from "@/components/Section";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-center text-primary text-7xl">Hello World!</h1>
-      <div className="flex bg-zinc-800	">
-        <h1 className="underline text-red-50 border-2 p-5 m-1">
-          HomePage testando
-        </h1>
-        <h1 className="underline text-red-50 border-2 p-5 m-1">
-          HomePage testando
-        </h1>
-      </div>
+    <div className="w-full border-8 border-purple-700">
+      <div className="text-center text-primary text-7xl bg-black p-4">Hello World!</div>
+      <Section
+        title={
+          <>
+            <div className="text-amber-500 text-xl">Oferta especial</div>
+            <div>Air Jordan edição de colecionador</div>
+          </>
+        }
+        link={{ text: "Ver oferta", href: "/productlist" }}
+        image={
+          <div className="relative h-64 mb-20">
+            <img
+              src="eclipse-roxa.png"
+              alt="fundo da imagem do sapato"
+              className="absolute inset-0 m-auto z-10 w-80 mt-2"
+            />
+            <img
+              src="air-jordan.png"
+              alt="sapato em promocao"
+              className="absolute inset-0 m-auto z-20 w-96 -mb-1"
+            />
+          </div>
+        }
+        className="py-8"
+      >
+        <div className="text-lg mb-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip
+        </div>
+      </Section>
     </div>
   );
 };
